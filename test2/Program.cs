a4cea9662a60af0cba95c51ea6e82ac4fe04ecc1
Константин <dosaevk@yandex.ru>
2022-12-01 01:44:12 +0300

    square, sqNext

--- test2/Program.cs
+++ test2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics.Metrics;
 using System.Globalization;
 using System.Linq;
@@ -16,9 +17,7 @@ namespace test2
         static void Main(string[] args)
         {
 
-
-
-            //Console.WriteLine(Res.R4( "65a" ));
+            //Console.WriteLine(ResA.Multiply(-2));
             //foreach (var VARIABLE in GetNames(new int[,] { { 1, 2 }, { 3, 4 } }))
             //{
             //    Console.WriteLine(VARIABLE);
@@ -27,16 +26,57 @@ namespace test2
 
             //Console.WriteLine(ArrayPlusArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
 
+            //long num = 1082077497969070681;
+
+
+            //Console.WriteLine(Math.Sqrt(num)); Console.WriteLine((Math.Sqrt(num)+1) * ((Math.Sqrt(num) +1)));
+            ////Console.WriteLine(q);
+            Console.WriteLine(FindNextSquare(5049531132362809));
+            //Console.WriteLine(Math.Sqrt(1082077497969070681));Console.WriteLine(Math.Sqrt(4503599761588225));
 
         }
 
-        public static string Stb(string word)
+        //public static string Stb(string word)
+        //{
+
+
+        //}
+        
+
+        public static long FindNextSquare(long num)
         {
+            //if (num == 4503599627370496 && num == 5049531132362809) return -1;
 
-            return Regex.Replace(word, "egg", "");
+            long r = (long)Math.Sqrt(num);
+
+            Console.WriteLine(r);
+            Console.WriteLine((r+1)*(r+1));
+            decimal defNum = num;
+                 
+            var sq = SquereR(num);
+            Console.WriteLine(sq);
+            
+            if (sq - Math.Truncate(sq) != 0) return -1;
+            var t = (sq+ 1.0M) * (sq +1.0M);
+            return (long)(t);
+            
+
         }
-       
+        static decimal SquereR(decimal square)
+        {
+            if (square < 0) return 0;
+
+            decimal root = square / 3;
+            int i;
+            for (i = 0; i < 35; i++)
+                root = (root + square / root) / 2;
+            return root;
+        }
+
     }
+    //using System;
+    //using System.Text.RegularExpressions;
+
 
 
 
--- test2/ResA.cs
+++ test2/ResA.cs
@@ -2,12 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace test2
 {
     internal class ResA
     {
+        // возвести 5 в количество цифр в number е и умножить это на number
+        public static int Multiply(int number )
+        {
+            return number*(int)Math.Pow(5, number.ToString().Length);
+        }
 
+        //factorial
+        protected int FacRecursion(int value)
+        {
+            return value switch
+            {
+                1 => 1,
+                < 0 => 0,
+                0 => 1,
+                _ => value * FacRecursion(value - 1)
+            };
+        }
+
+
+
+        //найти ЦИФРЫ в строке и удалить
+        public static string? FilterNumbers(string str)
+        {
+            return new Regex(@"(\d)+").Replace(str, "");
+            //return string.Concat(str.Where(c => !char.IsNumber(c)));
+            //return new string(str.Where(c => !Char.IsDigit(c)).ToArray());
+            //return string.Concat(str.Where(c => !char.IsNumber(c)));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    // исправить счетчик
+    public class Counter
+    {
+        private int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            private set
+            {
+                _value = value;
+            }
+        }
+
+        public Counter()
+        {
+            Value = 0;
+        }
+
+        public void Increase()
+        {
+            ++Value;
+        }
+
+        public void Reset()
+        {
+            Value = 0;
+        }
     }
 }
